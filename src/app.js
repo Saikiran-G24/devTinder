3925d027c8edeaa13277409d7c651ca6a0c76fec
Saikiran-G24 <saikiran0756@gmail.com>
2024-09-30 09:58:28 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express=require('express')
 const app=express()
 
 
-app.get(/a/,(req,res)=>{
+app.get("/user/:userId/:username/:password",(req,res)=>{
+    console.log(req.params)
     res.send({firatname:"saikiran",lastname:"kudurupaka"})
 })
 
-app.listen(3000,()=>{
-    console.log("server is successfully listening on port 3000.....")
+
+app.listen(7777,()=>{
+    console.log("server is successfully listening on port 7777.....")
 })
 
