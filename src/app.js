470f69879114419d49b727d7f165508183cefd37
Saikiran-G24 <saikiran0756@gmail.com>
2024-09-30 09:19:15 +0530

    Testing  http methods on postman

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express=require('express')
 const app=express()
-//request handler
-app.use("/",(req,res)=>{
-    res.send('hello from parent-file')
+
+
+app.get(/a/,(req,res)=>{
+    res.send({firatname:"saikiran",lastname:"kudurupaka"})
 })
-app.use("/test/result",(req,res)=>{
-    res.send('successfully getting result from server')
-})
-app.use("/hello",(req,res)=>{
-    res.send('server responding to the request')
-})
+
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000.....")
 })
