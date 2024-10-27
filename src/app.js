b6576a4f3f753b1d94f28bb2a5d5fca13727b2b5
Saikiran-G24 <saikiran0756@gmail.com>
2024-10-28 01:58:25 +0530

    Handle errors End Of your code or Use try-catch method

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use('/',(err,req,res,next)=>{
+  console.log('1....')
+  res.status(500).send('something went wrong........')
+})
 app.use("/user/getUserData",(req,res,next)=>{
   throw new error('oijhgfedrftg')
 
@@ -9,15 +14,15 @@ app.use('/',(err,req,res,next)=>{
   console.log('2....')
   res.status(500).send('something went wrong........')
 })
-app.use("/user",(req,res,next)=>{
-  console.log('1....')
-  try {
-    throw new error('got an error in your code ')
-  } catch (error) {
-    res.status(401).send("request resouse is unauthorised....")
-  }
+// app.use("/user",(req,res,next)=>{
+//   console.log('1....')
+//   try {
+//     throw new error('got an error in your code ')
+//   } catch (error) {
+//     res.status(401).send("request resouse is unauthorised....")
+//   }
   
-})
+// })
 
 app.listen(7777, () => {
   console.log("server is successfully listening on port 7777.....");
