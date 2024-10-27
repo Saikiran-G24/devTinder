abc05d719971d2c9a1979e18c01567e3e9994d7d
Saikiran-G24 <saikiran0756@gmail.com>
2024-10-28 01:53:18 +0530

    Error-Handling Of All Routs

--- src/app.js
+++ src/app.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth");
+app.use("/user/getUserData",(req,res,next)=>{
+  throw new error('oijhgfedrftg')
 
-app.use("/admin", adminAuth, (req, res, next) => {
-  console.log("All admin routes...");
-  // res.send("it will takecare of all the admin routes****");
-  next();
-});
-app.get("/admin/deletedata", (req, res, next) => {
-  res.send("deleting the admin data from server..");
-}),
-  app.post("/admin/getData", (req, res, next) => {
-    console.log("getting the admin data");
-    res.send("getting the admin data");
-  }),
-  app.post("/user", userAuth, (req, res, next) => {
-    res.send("sending user data to server....");
-  }),
-  app.get("/user/login", (req, res, next) => {
-    res.send("server is responding to your login request......");
-  });
+})
+app.use('/',(err,req,res,next)=>{
+  console.log('2....')
+  res.status(500).send('something went wrong........')
+})
+app.use("/user",(req,res,next)=>{
+  console.log('1....')
+  try {
+    throw new error('got an error in your code ')
+  } catch (error) {
+    res.status(401).send("request resouse is unauthorised....")
+  }
+  
+})
+
 app.listen(7777, () => {
   console.log("server is successfully listening on port 7777.....");
 });
